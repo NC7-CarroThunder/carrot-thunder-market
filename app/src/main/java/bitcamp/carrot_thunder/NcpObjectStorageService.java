3f46f7414d96e238d2215cc795b4008a7cb18d84
Romangduck <rrlagusejr@naver.com>
2023-10-10 07:49:30 +0900

    Modify NcpOSS for adding deletefiles , add Post parts
    NCP 서비스파일 삭제 첨부 , 게시글 정보조회 수정 , Post 의 사용될
    Response Request 객체 구현, 안쓰여지는 사진들 삭제하는 기능 구현중

--- app/src/main/java/bitcamp/carrot_thunder/NcpObjectStorageService.java
+++ app/src/main/java/bitcamp/carrot_thunder/NcpObjectStorageService.java
@@ -1,18 +1,20 @@
 package bitcamp.carrot_thunder;
 
 import bitcamp.carrot_thunder.config.NcpConfig;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import java.io.InputStream;
-import java.util.UUID;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.UUID;
 
 @Component
 public class NcpObjectStorageService {
@@ -22,11 +24,11 @@ public class NcpObjectStorageService {
   public NcpObjectStorageService(NcpConfig ncpConfig) {
     System.out.println("✅NcpObjectStorageService() executed");
     s3 = AmazonS3ClientBuilder.standard()
-        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-            ncpConfig.getEndPoint(), ncpConfig.getRegionName()))
-        .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
-            ncpConfig.getAccessKey(), ncpConfig.getSecretKey())))
-        .build();
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                    ncpConfig.getEndPoint(), ncpConfig.getRegionName()))
+            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                    ncpConfig.getAccessKey(), ncpConfig.getSecretKey())))
+            .build();
   }
 
   public String uploadFile(String bucketName, String dirPath, MultipartFile part) {
@@ -41,10 +43,10 @@ public class NcpObjectStorageService {
       objectMetadata.setContentType(part.getContentType());
 
       PutObjectRequest objectRequest = new PutObjectRequest(
-          bucketName,
-          dirPath + filename,
-          fileIn,
-          objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead);
+              bucketName,
+              dirPath + filename,
+              fileIn,
+              objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead);
 
       s3.putObject(objectRequest);
 
@@ -52,6 +54,14 @@ public class NcpObjectStorageService {
 
     } catch (Exception e) {
       throw new RuntimeException("❗️파일 업로드 오류", e);
+    }
+  }
+
+  public void deleteFile(String bucketName, String filePath) {
+    try {
+      s3.deleteObject(new DeleteObjectRequest(bucketName, filePath));
+    } catch (Exception e) {
+      throw new RuntimeException("❗️파일 삭제 오류", e);
     }
   }
 }
---
+++ app/src/main/java/bitcamp/carrot_thunder/post/dto/PostRequestDto.java
@@ -0,0 +1,21 @@
+package bitcamp.carrot_thunder.post.dto;
+
+import bitcamp.carrot_thunder.post.model.vo.AttachedFile;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PostRequestDto {
+
+  private String title;
+  private String content;
+  private String itemCategory;
+  private String dealingType;
+  private String itemStatus;
+  private String address;
+  private int price;
+  private List<AttachedFile> attachedFilesPaths;
+
+//이걸로게시글작성
+}
