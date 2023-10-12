84b81bbd04134270e29f788bad966157086b82b7
dongyoungs <kkssckkssc#gmail.com>
2023-10-12 14:39:20 +0900

    ﻿컴파일에러 수정

--- app/src/main/java/bitcamp/carrot_thunder/user/model/dao/UserDao.java
+++ app/src/main/java/bitcamp/carrot_thunder/user/model/dao/UserDao.java
@@ -9,33 +9,46 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface UserDao {
 
+  int insert(User user);
 
-  int insert (User user);
   // 회원 가입
   int signup(User user);
+
   List<User> findAll();
+
   User findBy(Long userId);
+
   // 닉네임 중복 검사
   User findByNickName(String nickname);
+
   // 이메일 중복 검사
   User findByEmail(String email);
+
   User findByEmailAndPassword(
       @Param("email") String email,
       @Param("password") String password);
 
   int update(User user);
+
   void updatePasswordByName(String nickName, String password);
+
   int delete(Long userId);
 
-
   void insertFollow(Long followerId, Long followingId);
+
   void deleteFollow(Long followerId, Long followingId);
+
   boolean isFollowed(Long followerId, Long followingId);
+
   List<User> getFollowers(Long userId);
+
   List<User> getFollowings(Long userId);
 
   int insertNotification(Notification notification);
+
   int updateReadStatus(int id, boolean isRead);
+
   List<Notification> findNotificationsByUserId(Long userId);
+
   void deleteAllNotifications(Long userId) throws Exception;
 }
--- app/src/main/java/bitcamp/carrot_thunder/user/service/UserService.java
+++ app/src/main/java/bitcamp/carrot_thunder/user/service/UserService.java
@@ -13,32 +13,40 @@ import javax.servlet.http.HttpSession;
 
 public interface UserService {
 
-    public String patchPassword(UserDetailsImpl userDetails, String password) throws Exception;
-    String login(LoginRequestDto loginInfo, HttpServletResponse response) throws Exception;
+  public String patchPassword(UserDetailsImpl userDetails, String password) throws Exception;
 
-    String signup(SignupRequestDto signupRequestDto, HttpServletResponse response) throws Exception;
-     int add(User user) throws Exception;
-    List<User> list() throws Exception;
-    User get(Long userId) throws Exception;
-    User get(String email, String password) throws Exception;
-    User get(String email) throws Exception;
-    int update(User user) throws Exception;
+  String login(LoginRequestDto loginInfo, HttpServletResponse response) throws Exception;
 
+  String signup(SignupRequestDto signupRequestDto, HttpServletResponse response) throws Exception;
 
-    void updatePasswordByName(String nickName, String password) throws Exception;
+  int add(User user) throws Exception;
 
+  List<User> list() throws Exception;
 
+  User get(Long userId) throws Exception;
 
-    boolean memberFollow(Long followerId, Long followingId) throws Exception;
-    boolean isFollowed(Long followerId, Long followingId) throws Exception;
-    List<User> getFollowers(Long userId) throws Exception;
-    List<User> getFollowings(Long userId) throws Exception;
+  User get(String email, String password) throws Exception;
 
-    @Transactional
-    int delete(Long userId) throws Exception;
+  User get(String email) throws Exception;
 
-    User get(Long userId, HttpSession session) throws  Exception;
+  int update(User user) throws Exception;
 
-    List<Notification> getNotifications(Long userId) throws Exception;
-    void deleteAllNotifications(Long userId) throws Exception;
+  void updatePasswordByName(String nickName, String password) throws Exception;
+
+  boolean memberFollow(Long followerId, Long followingId) throws Exception;
+
+  boolean isFollowed(Long followerId, Long followingId) throws Exception;
+
+  List<User> getFollowers(Long userId) throws Exception;
+
+  List<User> getFollowings(Long userId) throws Exception;
+
+  @Transactional
+  int delete(Long userId) throws Exception;
+
+  User get(Long userId, HttpSession session) throws Exception;
+
+  List<Notification> getNotifications(Long userId) throws Exception;
+
+  void deleteAllNotifications(Long userId) throws Exception;
 }
