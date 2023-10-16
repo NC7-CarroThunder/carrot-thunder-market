d0abfa610c3c782a2ea26433ffaf69b35a02c6c5
Romangduck <rrlagusejr@naver.com>
2023-10-16 19:06:30 +0900

    Modify merging mistake

--- app/src/main/java/bitcamp/carrot_thunder/post/dto/PostResponseDto.java
+++ app/src/main/java/bitcamp/carrot_thunder/post/dto/PostResponseDto.java
@@ -5,7 +5,7 @@ import bitcamp.carrot_thunder.post.model.vo.DealingType;
 import bitcamp.carrot_thunder.post.model.vo.ItemCategory;
 import bitcamp.carrot_thunder.post.model.vo.ItemStatus;
 import bitcamp.carrot_thunder.post.model.vo.Post;
-
+import bitcamp.carrot_thunder.user.model.vo.User;
 import java.sql.Timestamp;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -20,43 +20,44 @@ import lombok.NoArgsConstructor;
 
 public class PostResponseDto {
 
-  private Long postid;
-  private String title;
-  private String content;
-  private int viewCount;
-  private Timestamp createdAt;
-  private ItemCategory itemCategory;
-  private ItemStatus itemStatus;
-  private DealingType dealingType;
-  private String address;
-  private int price;
-  private String nickname;
-  private int likeCount;
-  private List<AttachedFile> attachedFilesPaths;
-  private Boolean isLiked;
-  private Long userid;
+    private User user;
+    private Long postId;
+    private String title;
+    private String content;
+    private int viewCount;
+    private Timestamp createdAt;
+    private ItemCategory itemCategory;
+    private ItemStatus itemStatus;
+    private DealingType dealingType;
+    private String address;
+    private int price;
+    private String nickname;
+    private int likeCount;
+    private List<AttachedFile> attachedFilesPaths;
+    private Boolean isLiked;
+    private Long userid;
 
-  public static PostResponseDto of(Post post) {
-    return PostResponseDto.builder()
-            .postid(post.getId())
-            .title(post.getTitle())
-            .content(post.getContent())
-            .price(post.getPrice())
-            .nickname(post.getUser().getNickName())
-            .userid(post.getUser().getId())
-            .isLiked(post.isLiked())
-            .viewCount(post.getViewCount())
-            .itemCategory(post.getItemCategory())
-            .dealingType(post.getDealingType())
-            .itemStatus(post.getItemStatus())
-            .createdAt(post.getCreatedAt())
-            .address(post.getAddress())
-            .likeCount(post.getLikeCount())
-            .attachedFilesPaths(post.getAttachedFiles())
-            .build();
-  }
+    public static PostResponseDto of(Post post) {
+        return PostResponseDto.builder()
+                .postId(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .price(post.getPrice())
+                .nickname(post.getUser().getNickName())
+                .userid(post.getUser().getId())
+                .isLiked(post.isLiked())
+                .viewCount(post.getViewCount())
+                .itemCategory(post.getItemCategory())
+                .dealingType(post.getDealingType())
+                .itemStatus(post.getItemStatus())
+                .createdAt(post.getCreatedAt())
+                .address(post.getAddress())
+                .likeCount(post.getLikeCount())
+                .attachedFilesPaths(post.getAttachedFiles())
+                .build();
+    }
 
-  public void setAttachedFiles(List<AttachedFile> attachedFilesPaths) {
-    this.attachedFilesPaths = attachedFilesPaths;
-  }
+    public void setAttachedFiles(List<AttachedFile> attachedFilesPaths) {
+        this.attachedFilesPaths = attachedFilesPaths;
+    }
 }
